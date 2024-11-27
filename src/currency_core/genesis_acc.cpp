58a89584937547a790c86c8be97a8d71194ac9db
ArqTras <biuro@ar-net.info>
2024-11-27 13:23:15 +0000

    init block

--- src/currency_core/genesis_acc.cpp
+++ src/currency_core/genesis_acc.cpp
@@ -10,22 +10,19 @@
 namespace currency
 {
 #ifndef TESTNET
-  const std::string ggenesis_tx_pub_key_str = "";
-  const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
-  const genesis_tx_dictionary_entry ggenesis_dict[2] = {
-  //  { 898363347618325980ULL,7 },
-  //  { 17472133472787764818ULL,10 }
-  };
+    const std::string ggenesis_tx_pub_key_str = "7b38c5333c03270426f8ca3dde193cad6c415204e14cacebd3de7f652cd774e1";
+    const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
+    extern const genesis_tx_dictionary_entry ggenesis_dict[1];
+    const genesis_tx_dictionary_entry ggenesis_dict[1] = {
+    {11874369273325982160ULL,0}
+    };
 #else
-  const std::string ggenesis_tx_pub_key_str    = "";
-  const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
-  const genesis_tx_dictionary_entry ggenesis_dict[5] = {
-  //  { 4413532107669521528ULL, 2 },
-  //  { 4848259848862559835ULL, 4 },
-  //  { 4891306118630423916ULL, 1 },
-  //  { 6536034028979999929ULL, 0 },
-  //  { 15528122346224653564ULL, 3 }
-};
+    const std::string ggenesis_tx_pub_key_str = "7b38c5333c03270426f8ca3dde193cad6c415204e14cacebd3de7f652cd774e1";
+    const crypto::public_key ggenesis_tx_pub_key = epee::string_tools::parse_tpod_from_hex_string<crypto::public_key>(ggenesis_tx_pub_key_str);
+    extern const genesis_tx_dictionary_entry ggenesis_dict[1];
+    const genesis_tx_dictionary_entry ggenesis_dict[1] = {
+    {11874369273325982160ULL,0}
+    };
 #endif
 
 
