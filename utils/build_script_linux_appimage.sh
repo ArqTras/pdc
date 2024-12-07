6a61900a4034e71f39ddf5a3005809b2082ea2ff
ArqTras <biuro@ar-net.info>
2024-12-07 03:38:31 +0100

    Update build_script_linux_appimage.sh
    appimage

--- utils/build_script_linux_appimage.sh
+++ utils/build_script_linux_appimage.sh
@@ -47,10 +47,10 @@ else
 echo "---------------- BUILDING PROJECT ----------------"
 echo "--------------------------------------------------"
 
-echo "Building...." 
+echo "Building...."
 
-rm -rf build; mkdir -p build/release; 
-cd build/release; 
+rm -rf build; mkdir -p build/release;
+cd build/release;
 cmake $testnet_def -D STATIC=true -D ARCH=x86-64 -D DISABLE_TOR=TRUE  -D BUILD_GUI=TRUE -D OPENSSL_ROOT_DIR="$OPENSSL_ROOT_DIR" -D CMAKE_PREFIX_PATH="$QT_PREFIX_PATH" -D CMAKE_BUILD_TYPE=Release ../..
 if [ $? -ne 0 ]; then
     echo "Failed to run cmake"
@@ -63,7 +63,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-make -j1 Zano
+make -j1 Pdc
 if [ $? -ne 0 ]; then
     echo "Failed to make!"
     exit 1
